7ec147f3632f6f29e89b1d7ee871789dfe425d12
Gokulsarvesh <gokulsarvesh@zed.digital>
2024-05-17 12:59:37 +0530

    import changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 # Load the tokenizers
 with open('tokenizer.pkl', 'rb') as f:
